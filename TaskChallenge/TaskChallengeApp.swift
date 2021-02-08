62bc05520e72c745fa99794353b26327591de951
ひろろ <hrsi.ad@gmail.com>
2021-02-08 11:07:26 +0900

    Initial Commit

---
+++ TaskChallenge/TaskChallengeApp.swift
@@ -0,0 +1,17 @@
+//
+//  TaskChallengeApp.swift
+//  TaskChallenge
+//
+//  Created by Hiroshi.Nakai on 2021/02/08.
+//
+
+import SwiftUI
+
+@main
+struct TaskChallengeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
