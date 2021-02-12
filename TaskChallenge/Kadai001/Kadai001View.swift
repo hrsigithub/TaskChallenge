6f0d1c0a5b63695f4984976404be064a6cb561f5
ひろろ <hrsi.ad@gmail.com>
2021-02-12 10:35:37 +0900

    okaさんFB反映

--- TaskChallenge/Kadai001/Kadai001View.swift
+++ TaskChallenge/Kadai001/Kadai001View.swift
@@ -15,31 +15,42 @@ struct Kadai001View: View {
     @State private var result = ""
 
     var body: some View {
-        VStack {
-            InputView(text: $textArray[0])
-            InputView(text: $textArray[1])
-            InputView(text: $textArray[2])
-            InputView(text: $textArray[3])
-            InputView(text: $textArray[4])
+        HStack {
+            VStack {
+                VStack(spacing: 20.0){
+                    InputView(text: $textArray[0])
+                    InputView(text: $textArray[1])
+                    InputView(text: $textArray[2])
+                    InputView(text: $textArray[3])
+                    InputView(text: $textArray[4])
 
-            Text(result)
-            
-            Button(action: {
-                // 計算
-                let num1 = Int(textArray[0]) ?? 0
-                let num2 = Int(textArray[1]) ?? 0
-                let num3 = Int(textArray[2]) ?? 0
-                let num4 = Int(textArray[3]) ?? 0
-                let num5 = Int(textArray[4]) ?? 0
-                
-                let total = num1 + num2 + num3 + num4 + num5
-                self.result = String(total)
-            }) {
-                Text("計算")
-            }.padding()
-
+//                    Text(result)
+                    
+                    Button(action: {
+                        // 計算
+                        let num1 = Int(textArray[0]) ?? 0
+                        let num2 = Int(textArray[1]) ?? 0
+                        let num3 = Int(textArray[2]) ?? 0
+                        let num4 = Int(textArray[3]) ?? 0
+                        let num5 = Int(textArray[4]) ?? 0
+                        
+                        let total = num1 + num2 + num3 + num4 + num5
+                        self.result = String(total)
+                        
+                        UIApplication.shared.closeKeyboard()
+                    }) {
+                        Text("計算")
+                    }.padding()
+                    HStack {
+                        Text(result)
+                   }
+                   .frame(width: 150, height: 30, alignment: .leading)
+                    Spacer()
+                }
+            }
             Spacer()
         }
+        .padding(.leading, 20.0)
     }
 }
 
@@ -50,9 +61,10 @@ struct InputView: View {
         TextField("", text: $text)
             .overlay(
                 RoundedRectangle(cornerRadius: 5)
-                    .stroke(Color.blue, lineWidth: 1)
+//                    .stroke(Color.blue, lineWidth: 1)
+                    .stroke(Color.black.opacity(0.5), lineWidth: 1)
             )
-            .padding()
+//            .padding()
     }
 }
 
@@ -61,3 +73,10 @@ struct Kadai001View_Previews: PreviewProvider {
         Kadai001View()
     }
 }
+
+extension UIApplication {
+    func closeKeyboard() {
+        sendAction(#selector(UIResponder.resignFirstResponder), to: nil, from: nil, for: nil)
+    }
+}
+
